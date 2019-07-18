cec67505b78272d05af40cc64c8ce46616c0249e
Alice <alice.leon10@gmail.com>
2019-07-18 16:18:54 +0200

    ...

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger        = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 
+const request = require('request');
 var mongodb       = require('./db');
 
 var routes = require('./routes/routes');
